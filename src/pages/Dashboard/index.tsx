ab133ae0bf3c3cc15938c32d0dfa53e589d54be6
Daniel Souza <daniell.oliveirra@gmail.com>
2020-09-02 17:14:27 -0300

    Pesquisar respositorios github

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,32 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import { Title, Form, Repositories } from './styles';
 import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={logoImg} alt="Logo - GitHub Explore" />
-    <Title> Explore repositórios no GitHub </Title>
-    <Form>
-      <input placeholder="Digite aqui" />
-      <button type="submit"> Pesquisar </button>
-    </Form>
+interface Repository {
+  fullname: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
 
-    <Repositories>
-      <a href="/">
-        <img
-          src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
-          alt="Teste"
+const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState< Repository[] >([]);
+  const [newRepo, setNewRepo] = useState('');
+
+  async function handleAddRepository(event: React.FormEvent<HTMLFormElement>):Promise<void> {
+    event.preventDefault();
+
+    const { data: repository } = await api.get(`/repos/${newRepo}`);
+    setRepositories([...repositories, repository]);
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="Logo - GitHub Explore" />
+      <Title> Explore repositórios no GitHub </Title>
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite aqui"
         />
+        <button type="submit"> Pesquisar </button>
+      </Form>
 
-        <div>
-          <strong> Auuuuuuuuuuuuuuuuuuuu </strong>
-          <p>ggdrnjsksjn</p>
-        </div>
-        <FiChevronRight size="20" />
-      </a>
-    </Repositories>
-  </>
-);
+      <Repositories>
+        <a href="/">
+          <img
+            src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+            alt="Teste"
+          />
+
+          <div>
+            <strong> Auuuuuuuuuuuuuuuuuuuu </strong>
+            <p>ggdrnjsksjn</p>
+          </div>
+          <FiChevronRight size="20" />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
